6b12a3c51c50a3f41a6ba953519c2940e5ed6f33
BrunoPracidiu <fo.bruno@gmail.com>
2018-09-08 17:17:44 +1000

    Create users components
    Create users components

--- src/Users/Users.tsx
+++ src/Users/Users.tsx
@@ -4,27 +4,24 @@ import * as React from "react"
 import { Text, View, ScrollView, StyleSheet, Button } from "react-native"
 import { styles } from './Users.style'
 
-class App extends Component{
-
-state = {
-    ListUsers: var
+export interface Props {
+    users: Users[],
 }
 
 export function Users() {
-    return (
-        <ScrollView horizontal contentContainerStyle={{flexGrow: 1}}>
-            <View style={styles.alightViewWithPadings}>
-                <View style={[styles.backgroundMidle, styles.colorBlue1]}>
-                    <Text style={styles.textAlightCenter}>Adiwena</Text>
-                </View>
-                <View style={[styles.backgroundMidle, styles.colorBlue2]}>
-                    <Text style={styles.textAlightCenter}>Adam</Text>
-                </View>
-                <View style={[styles.backgroundMidle, styles.colorBlue3]}>
-                    <Text style={styles.textAlightCenter}>Bruno</Text>
-                </View>   
+return (
+    <ScrollView horizontal contentContainerStyle={{flexGrow: 1}}>
+        <View style={styles.alightViewWithPadings}>
+            <View style={[styles.backgroundMidle, styles.colorBlue1]}>
+                <Text style={styles.textAlightCenter}>Adi</Text>
             </View>
-        </ScrollView>
-    )
-}
+            <View style={[styles.backgroundMidle, styles.colorBlue2]}>
+                <Text style={styles.textAlightCenter}>Adam</Text>
+            </View>
+            <View style={[styles.backgroundMidle, styles.colorBlue3]}>
+                <Text style={styles.textAlightCenter}>Bruno</Text>
+            </View>   
+        </View>
+    </ScrollView>
+)
 }
