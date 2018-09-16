69d3660138edffcc4fabe9628ea8c6ee1c9a8cbf
Adiwena Kurnadi <adiwena.kurnadi@ascsoftware.com.au>
2018-09-17 00:56:31 +1000

    handle month selection

--- src/MonthView/phone/MonthSelection/MonthSelection.tsx
+++ src/MonthView/phone/MonthSelection/MonthSelection.tsx
@@ -1,61 +1,108 @@
+import { Moment } from "moment"
+import moment from "moment"
 import * as React from "react"
 import { Component } from "react"
+import { Image, Text, TouchableHighlight, View } from "react-native"
+import { Month } from "../../../shared/model"
+import { styles } from "./MonthSelection.styles"
 
-import { Text, TouchableHighlight, View } from "react-native"
+enum MonthAsString {
+    January = "January",
+    February = "February",
+    March = "March",
+    April = "April",
+    May = "May",
+    June = "June",
+    July = "July",
+    August = "August",
+    September = "September",
+    October = "October",
+    November = "November",
+    December = "December"
+}
 
 interface Props {
-    handleChangeMonth: (month: Month) => void
+    onMonthChange?: (month: Month, year: number) => void
+    currentYear?: number
 }
 
 interface State {
     currentMonth: Month
-    currentIndex: number
+    currentYear: number
 }
 
 export class MonthSelection extends Component<Props, State> {
-    private allMonths: Month[] = [Month.January, Month.February, Month.March, Month.April, Month.May,
-        Month.June, Month.July, Month.August, Month.September, Month.October, Month.November, Month.December]
+    private allMonths: MonthAsString[] = [MonthAsString.January, MonthAsString.February,
+        MonthAsString.March, MonthAsString.April, MonthAsString.May,
+        MonthAsString.June, MonthAsString.July, MonthAsString.August,
+        MonthAsString.September, MonthAsString.October, MonthAsString.November, MonthAsString.December]
+
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            currentMonth: Month.January,
+            currentYear: props.currentYear || moment().year()
+        }
+    }
+
+    onMonthChange = (month: Month, year: number) => {
+        if (this.props.onMonthChange) {
+            this.props.onMonthChange(month, year)
+        }
+    }
 
     handlePreviousButtonPress = () => {
-        let { currentIndex } = this.state
-        if (currentIndex > 0) {
-            currentIndex -= 1
-            const currentMonth = this.allMonths[currentIndex]
-            this.setState({
-                currentIndex,
-                currentMonth
-            })
+        let { currentMonth, currentYear } = this.state
+        if (currentMonth > 0) {
+            currentMonth -= 1
+        } else {
+            currentYear -= 1
+            currentMonth = Month.December
         }
+
+        this.setState({
+            currentMonth,
+            currentYear
+        })
+
+        this.onMonthChange(currentMonth, currentYear)
     }
 
     handleNextButtonPress = () => {
-        let { currentIndex } = this.state
-        if (currentIndex < this.allMonths.length - 1) {
-            currentIndex += 1
-            const currentMonth = this.allMonths[currentIndex]
-            this.setState({
-                currentIndex,
-                currentMonth
-            })
+        let { currentMonth, currentYear } = this.state
+        if (currentMonth < this.allMonths.length - 1) {
+            currentMonth += 1
+        } else {
+            currentYear += 1
+            currentMonth = Month.January
         }
+
+        this.setState({
+            currentMonth,
+            currentYear
+        })
+
+        this.onMonthChange(currentMonth, currentYear)
     }
 
     render() {
+        const title = `${this.allMonths[this.state.currentMonth]} ${this.state.currentYear}`
         return(
-            <View>
-                <View>
-                    <TouchableHighlight onPress={this.handlePreviousButtonPress}>
-                        <text>{"<"}</text>
-                    </TouchableHighlight>
+            <View style={styles.container}>
+                <TouchableHighlight
+                    onPress={this.handlePreviousButtonPress}
+                    style={styles.containerButton}
+                >
+                    <Image source={require("./asset/arrow-back.png")} resizeMode={"center"}/>
+                </TouchableHighlight>
+                <View style={styles.containerTitle}>
+                    <Text style={styles.title}>{title}</Text>
                 </View>
-                <View>
-                    <Text>{this.state.currentMonth}</Text>
-                </View>
-                <View>
-                    <TouchableHighlight onPress={this.handleNextButtonPress}>
-                        <text>{">"}</text>
-                    </TouchableHighlight>
-                </View>
+                <TouchableHighlight
+                    onPress={this.handleNextButtonPress}
+                    style={styles.containerButton}>
+                    <Image source={require("./asset/arrow-forward.png")} resizeMode={"center"}/>
+                </TouchableHighlight>
             </View >
         )
     }
--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,22 @@
 import { Component } from "react"
 import * as React from "react"
 import { View } from "react-native"
+import { MonthSelection } from "./MonthView/phone/MonthSelection/MonthSelection"
+import { Month } from "./shared/model"
 
 export class App extends Component<any, any> {
 	render() {
 		return (
-			<View />
+			<View style={{
+				flex: 1,
+				marginTop: 200,
+				marginLeft: 50,
+				marginRight: 50
+			}} >
+				<MonthSelection onMonthChange={(month: Month, year: number) => {
+					// tslint:disable-next-line:no-console
+					console.log(`${month} - ${year}`)}}/>
+			</View >
 		)
 	}
 }
