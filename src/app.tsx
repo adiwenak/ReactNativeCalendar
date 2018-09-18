858c6ef237bd98e2ad5d2075557455994ef67d87
Adiwena Kurnadi <adiwena.kurnadi@ascsoftware.com.au>
2018-09-18 22:52:02 +1000

    remove MonthSelection from app tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,6 @@
 import { Component } from "react"
 import * as React from "react"
 import { View } from "react-native"
-import { MonthSelection } from "./MonthView/phone/MonthSelection/MonthSelection"
-import { Month } from "./shared/model"
 
 export class App extends Component<any, any> {
 	render() {
