a3e19d947756112bd761ff427e5b8659327b3f67
BrunoPracidiu <fo.bruno@gmail.com>
2018-09-20 21:40:04 +1000

    Change the listUsers
    Change the listUsers

--- src/Home/home.tsx
+++ src/Home/home.tsx
@@ -2,10 +2,9 @@ import { Component } from "react"
 import * as React from "react"
 import { Text, View, ScrollView, StyleSheet } from "react-native"
 import { getAllCoins, HomeActions } from "./home.actions"
-import { UserModel } from '../UsersView/UserModel';
+import { UserModel } from '../UsersView/usermodel';
 import { Users } from '../UsersView/Users';
 import { styles } from '../UsersView/Users.style';
-import { any } from 'prop-types';
 
 interface ComponentState {
     userSelected: UserModel[]
@@ -24,21 +23,12 @@ export class Home extends Component<ComponentProps, ComponentState> {
             userSelected : [],
             listUsers : [new UserModel("Bruno", "powderblue", true), 
                          new UserModel("Adi", "skyblue", true), 
+                         new UserModel("Adi", "skyblue", true), 
+                         new UserModel("Adi", "skyblue", true), 
+                         new UserModel("Adi", "skyblue", true), 
+                         new UserModel("Adi", "skyblue", true), 
+                         new UserModel("Adi", "skyblue", true), 
                          new UserModel("Adam", "steelblue", true)]
-        }
-    }
-
-    userChangeEvent = (users: UserModel) => {
-        var containUser = false
-        this.state.userSelected.forEach(x => {
-            if(x.name.trim == users.name.trim){
-                containUser = true
-            }
-        });
-        if (containUser){
-            this.setState({userSelected: this.state.userSelected.filter(x => x.name !== users.name)})    
-        }else{
-            this.setState({userSelected: this.state.userSelected.concat(users)})
         }
     }
 
@@ -47,10 +37,16 @@ export class Home extends Component<ComponentProps, ComponentState> {
             <View>
                 <View style={{backgroundColor:'#1111', height: 60}}></View>
                 <Users  userCollection={this.state.listUsers} 
-                        userChangeEvent={this.userChangeEvent}/>
+                        userSelectCollection={[]}
+                        onSelectedEvent={this.onSelectedEvent}/>
             </View>
         )
     }
+
+    onSelectedEvent(selectedUser: UserModel[]){
+        console.log(selectedUser)
+    }
+
 }
 
 
--- src/UsersView/Users.tsx
+++ src/UsersView/Users.tsx
@@ -7,35 +7,56 @@ import { UserModel } from './UserModel';
 
 interface ComponentProps {
     userCollection: UserModel[]
-    userChangeEvent: (lsSelectedUser: UserModel) => void
+    userSelectCollection: UserModel[]
+    onSelectedEvent: (selectedUser: UserModel[]) => void
 }
 
-export class Users extends Component<ComponentProps>
+interface SelectedUser {
+    [key: number]: UserModel
+}
+
+interface ComponentState {
+    selectedUser: SelectedUser
+}
+
+export class Users extends Component<ComponentProps, ComponentState>
 {
     constructor(props: any){
         super(props)
-        this.state = { userSelected : []}
+        this.state = {
+            selectedUser: {}
+        }
     }
 
-    handleUserChange = (user: UserModel) => {
-        user.pressStatus = !user.pressStatus
-        this.props.userChangeEvent(user)
-    }
-    
     renderButtons() {
         if (this.props.userCollection){
-            return this.props.userCollection!.map(x => {
+            return this.props.userCollection!.map((x: UserModel, idx: number) => {
                 return  <View style={[styles.backgroundMidle]}> 
                             <View style={{backgroundColor:x.pressStatus ? x.colour : 'white'}}>
                             <Button 
                                     color={x.pressStatus ? 'white' : 'black'} 
                                     title={x.name} 
-                                    onPress={() => { this.handleUserChange(x)}} />
+                                    onPress={() => { this.selectUser(x, idx)}} />
                             </View>
                         </View>
             })
         }
         return
+    }
+
+    selectUser = (user: UserModel, idx: number) => {
+        const newSelectedUser = { ...this.state.selectedUser }
+        user.pressStatus = !user.pressStatus
+        if (newSelectedUser[idx]) {
+            delete newSelectedUser[idx]
+        } else {
+            newSelectedUser[idx] = user
+        }
+
+        this.setState({ selectedUser: newSelectedUser })
+        const allValues = Object.keys(newSelectedUser).map((key: any)=> newSelectedUser[key])
+        
+        this.props.onSelectedEvent(allValues)
     }
 
     render(){
